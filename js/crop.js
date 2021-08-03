485aabd53cc8be43522519bc9498bbb19a79b102
Niclas Bartsch <niclas.bartsch@smail.inf.h-brs.de>
2021-08-03 19:53:39 +0200

    Fix indentation

--- js/crop.js
+++ js/crop.js
@@ -9,34 +9,34 @@ function convert() {
     reader.readAsArrayBuffer(file);
     
     reader.onload = async function() {
-    // Load PDF Document
-    const pdfDoc = await PDFDocument.load(reader.result);
-    
-    // Crop to ticket
-    var page = pdfDoc.getPage(0);
-    page.setCropBox(56, 577, 457, 230);
+        // Load PDF Document
+        const pdfDoc = await PDFDocument.load(reader.result);
+        
+        // Crop to ticket
+        var page = pdfDoc.getPage(0);
+        page.setCropBox(56, 577, 457, 230);
 
-    // Edit metadata
-    pdfDoc.setTitle('Semesterticket NRW/VRS');
-    const viewerPrefs = pdfDoc.catalog.getOrCreateViewerPreferences();
-    viewerPrefs.setHideToolbar(true);
-    viewerPrefs.setHideMenubar(true);
-    viewerPrefs.setHideWindowUI(true);
-    viewerPrefs.setFitWindow(true);
-    viewerPrefs.setCenterWindow(true);
-    viewerPrefs.setDisplayDocTitle(true);
+        // Edit metadata
+        pdfDoc.setTitle('Semesterticket NRW/VRS');
+        const viewerPrefs = pdfDoc.catalog.getOrCreateViewerPreferences();
+        viewerPrefs.setHideToolbar(true);
+        viewerPrefs.setHideMenubar(true);
+        viewerPrefs.setHideWindowUI(true);
+        viewerPrefs.setFitWindow(true);
+        viewerPrefs.setCenterWindow(true);
+        viewerPrefs.setDisplayDocTitle(true);
 
-    // Save data
-    pdfBytes = await pdfDoc.save();
+        // Save data
+        pdfBytes = await pdfDoc.save();
 
-    // Show PDF and Download button
-    // pdfView.hidden = false;
-    downloadView.hidden = false;
-    saveBtn.disabled = false;
+        // Show PDF and Download button
+        // pdfView.hidden = false;
+        downloadView.hidden = false;
+        saveBtn.disabled = false;
     };
 
     reader.onerror = function() {
-    console.log(reader.error);
+        console.log(reader.error);
     };
 }
 
